d7472f7374f0ddf8b7155ad284d21d62af841387
BaguetteYeeter <baguetteyeeter@icloud.com>
2024-11-10 20:55:50 +0000

    added hello and libiconv

---
+++ packages/hello/build.sh
@@ -0,0 +1,12 @@
+preset=gnu
+name=hello
+version=2.12.1
+description="GNU Friendly greeting program"
+rundepends=libiconv
+checksum=8d99142afd92576f30b0cd7cb42a8dc6809998bc5d607d88761f512e26c7db20
+
+run_preset
+
+if [[ $(uname) == "Darwin" ]]; then
+    stage50="make CFLAGS=\"-liconv\""
+fi
---
+++ packages/libiconv/build.sh
@@ -0,0 +1,7 @@
+preset=gnu
+name=libiconv
+version=1.17
+description="GNU Conversion library"
+checksum=8f74213b56238c85a50a5329f77e06198771e70dd9a739779f4c02f65d971313
+
+run_preset
