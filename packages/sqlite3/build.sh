8cb1c4980c8d82536249b071d85f962ff92d6898
BaguetteYeeter <baguetteyeeter@icloud.com>
2024-11-14 17:00:51 +0000

    added sqlite3

---
+++ packages/sqlite3/build.sh
@@ -0,0 +1,12 @@
+preset=gnu-configure
+name=sqlite3
+version=3.47.0
+fileversion=3470000
+description="small SQL database engine"
+url=https://www.sqlite.org/2024/sqlite-autoconf-$fileversion.tar.gz
+checksum=83eb21a6f6a649f506df8bd3aab85a08f7556ceed5dbd8dea743ea003fc3a957
+
+run_preset
+
+stage00="cd sqlite-autoconf-$fileversion"
+stage99="rm sqlite-autoconf-$fileversion"
