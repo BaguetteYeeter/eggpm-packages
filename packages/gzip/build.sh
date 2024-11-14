eccb913a54a3f268dbdd1331c01bfb673dde6be1
Baguette Yeeter <baguetteyeeter@icloud.com>
2024-11-14 11:37:48 +0000

    added gzip,xz

---
+++ packages/gzip/build.sh
@@ -0,0 +1,7 @@
+preset=gnu
+name=gzip
+version=1.13
+description="GNU compression program"
+checksum=20fc818aeebae87cdbf209d35141ad9d3cf312b35a5e6be61bfcfbf9eddd212a
+
+run_preset
---
+++ packages/xz/build.sh
@@ -0,0 +1,8 @@
+preset=gnu-configure
+name=xz
+version=5.6.2
+description="compression program"
+url=https://github.com//tukaani-project/xz/releases/download/v$version/xz-$version.tar.xz
+checksum=a9db3bb3d64e248a0fae963f8fb6ba851a26ba1822e504dc0efd18a80c626caf
+
+run_preset
