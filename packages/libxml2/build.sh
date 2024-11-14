6bd71763b36f89ea57fdc76a038e30d93efa7be7
Baguette Yeeter <baguetteyeeter@icloud.com>
2024-11-14 11:47:48 +0000

    added libxml2

---
+++ packages/libxml2/build.sh
@@ -0,0 +1,8 @@
+preset=gnu-configure
+name=libxml2
+version=2.13.5
+description="library for parsing XML"
+url=https://download.gnome.org/sources/libxml2/2.13/libxml2-$version.tar.xz
+checksum=74fc163217a3964257d3be39af943e08861263c4231f9ef5b496b6f6d4c7b2b6
+
+run_preset
