cf80c5b210b4753fe864538285e1fffa9ba57f67
Baguette Yeeter <baguetteyeeter@icloud.com>
2024-11-14 11:23:46 +0000

    added libarchive

---
+++ packages/libarchive/build.sh
@@ -0,0 +1,9 @@
+preset=gnu
+name=libarchive
+version=3.7.7
+description="a library for reading and writing streaming archives"
+checksum=879acd83c3399c7caaee73fe5f7418e06087ab2aaf40af3e99b9e29beb29faee
+
+run_preset
+
+url=https://github.com/libarchive/libarchive/releases/download/v$version/libarchive-$version.tar.xz
