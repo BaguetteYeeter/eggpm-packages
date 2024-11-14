28ca3a2528d8d74de8a5bd8199ddc3a95749fdff
Baguette Yeeter <baguetteyeeter@icloud.com>
2024-11-14 11:13:18 +0000

    added libpsl

---
+++ packages/libpsl/build.sh
@@ -0,0 +1,9 @@
+preset=gnu
+name=libpsl
+version=0.21.5
+description="Public Suffix List Library"
+checksum=1dcc9ceae8b128f3c0b3f654decd0e1e891afc6ff81098f227ef260449dae208
+
+run_preset
+
+url=https://github.com/rockdaboot/libpsl/releases/download/$version/libpsl-$version.tar.gz
