5893722fb9e76ee8dc13882cbe889b78f4f72ab4
BaguetteYeeter <baguetteyeeter@icloud.com>
2024-11-16 13:12:10 +0000

    fixed curl dependencies

--- packages/curl/build.sh
+++ packages/curl/build.sh
@@ -4,7 +4,7 @@ version=8.11.0
 description="command line tool and library for transferring data with URLs"
 url=https://curl.se/download/curl-$version.tar.xz
 checksum=db59cf0d671ca6e7f5c2c5ec177084a33a79e04c97e71cf183a5cdea235054eb
-dependencies="openssl libpsl"
+rundepends="openssl libpsl"
 
 run_preset
 
