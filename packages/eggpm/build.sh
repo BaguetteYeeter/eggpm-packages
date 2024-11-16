83617ae362542942114f7d67286fa1ce50b7ad3d
BaguetteYeeter <baguetteyeeter@icloud.com>
2024-11-16 18:55:00 +0000

    added eggpm

---
+++ packages/eggpm/build.sh
@@ -0,0 +1,9 @@
+preset=gnu-configure
+name=eggpm
+version=0.3
+description="EggPM Package Manager"
+rundepends="curl libarchive libxml2 openssl sqlite3 tar xz"
+url=https://github.com/BaguetteYeeter/$name/releases/download/v$version/$name-$version.tar.xz
+checksum=2939639f767478986e2058d47b89779e9230046d98f2b36599b81815789eebae
+
+run_preset
