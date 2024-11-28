32b67701a3594b01c941d3538f750a43ff1b914a
BaguetteYeeter <baguetteyeeter@icloud.com>
2024-11-28 19:11:44 +0000

    final glibc fixes

--- packages/glibc/build.sh
+++ packages/glibc/build.sh
@@ -6,11 +6,10 @@ checksum=2abc038f5022949cb67e996c3cae0e7764f99b009f0b9b7fd954dfc6577b599e
 
 run_preset
 
-stage01="mkdir build"
+stage01="mkdir -p build"
 stage02="cd build"
 stage20=".$stage20 --enable-stack-protector=strong --disable-nscd libc_cv_slibdir=/usr/lib"
 
-stage60="touch ../../build/etc/ld.so.conf"
-stage70="sed '/test-installation/s@$(PERL)@echo not running@' -i ../Makefile"
-
+stage70='sed "/test-installation/s@\$(PERL)@echo not running@" -i ../Makefile'
+stage80="make install DESTDIR=\$(pwd)/../../build"
 stage90="cd .."
