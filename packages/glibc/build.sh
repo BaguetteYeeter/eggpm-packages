aaf556eafd8b2710a09b5bf7a3e38e026e683241
BaguetteYeeter <baguetteyeeter@icloud.com>
2024-11-28 15:47:18 +0000

    fixed issue in glibc

--- packages/glibc/build.sh
+++ packages/glibc/build.sh
@@ -4,6 +4,8 @@ version=2.40
 description="GNU C library"
 checksum=2abc038f5022949cb67e996c3cae0e7764f99b009f0b9b7fd954dfc6577b599e
 
+run_preset
+
 stage01="mkdir build"
 stage02="cd build"
 stage20=".$stage20 --enable-stack-protector=strong --disable-nscd libc_cv_slibdir=/usr/lib"
